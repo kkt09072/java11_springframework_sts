a80bf450b11336f1a90ee0d335a42a84cb22a345
kkt09072 <ksb450424@naver.com>
2024-05-07 17:10:55 +0900

    Dependensy Injection & Invert of Control

--- study03/src/main/java/com/spring1/test/InjectionTest2.java
+++ study03/src/main/java/com/spring1/test/InjectionTest2.java
@@ -8,15 +8,15 @@ import com.spring1.dto.Shop;
 public class InjectionTest2 {
 
 	public static void main(String[] args) {
-	  ApplicationContext ctx1 = new GenericXmlApplicationContext("classpath:injectionContext2.xml");
+	  ApplicationContext ctx2 = new GenericXmlApplicationContext("classpath:injectionContext2.xml");
 		
- 	  Shop shop1 = ctx1.getBean("shop1", Shop.class);
+ 	  Shop shop1 = ctx2.getBean("shop1", Shop.class);
 	  System.out.println(shop1.toString());
 		
-	  Shop shop2 = (Shop) ctx1.getBean("shop2", Shop.class);
+	  Shop shop2 = (Shop) ctx2.getBean("shop2", Shop.class);
 	  System.out.println(shop2.toString());
 		
-	  Shop shop3 = (Shop) ctx1.getBean("shop3", Shop.class);
+	  Shop shop3 = (Shop) ctx2.getBean("shop3", Shop.class);
 	  System.out.println(shop3.toString());
 	}
 
